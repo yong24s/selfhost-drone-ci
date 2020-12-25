9d753a8a0c12faa895208b1a579d0c0f8757d91f
Yong Hao TENG <yonghao.teng@gmail.com>
2020-12-25 23:50:56 +0800

    Update drone-server@2255d63 and fix build.sh

--- drone-server/build/scripts/build.sh
+++ drone-server/build/scripts/build.sh
@@ -4,16 +4,16 @@ DRONE="/build/drone-server"
 
 if [ ! -f "$DRONE" ]; then
   CLONE_DIR="/tmp/clone"
-  GOPATH="/tmp/build"
+  GOPATH="/go"
 
   git clone https://github.com/drone/drone.git $CLONE_DIR
 
   # add oss to -tags as required
-  cd $CLONE_DIR && go install -tags "nolimit" github.com/drone/drone/cmd/drone-server
+  cd $CLONE_DIR && go install -tags "nolimit" $CLONE_DIR"/cmd/drone-server"
 
   mv $GOPATH"/bin/drone-server" $DRONE
 
-  rm -rf CLONE_DIR GOPATH
+  rm -rf CLONE_DIR
 fi
 
 # Change pwd to a mounted volume so that db file will be saved there
